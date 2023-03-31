9f2435692b8cf855f7b2b694bf44f5a8acf98286
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-31 11:56:12 -0300

    Terminando ejercicio adicional

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
-alert("Hora de jugar piedra papel o tijera!")
-let eleccion = ["Piedra", "Papel", "Tijera"]
-let usuarioEleccion = prompt("Piedra, papel o tijera?")
-let eleccionCantidad = eleccion.length
-let numeroAzar = Math.floor(Math.random() * eleccionCantidad)
-let compuEleccion = eleccion[numeroAzar]
+function comenzarJuego() {
+  let eleccion = ["Piedra", "Papel", "Tijera"]
+  let usuarioEleccion = document.getElementById("usuarioEleccion").value
+  let eleccionCantidad = eleccion.length
+  let numeroAzar = Math.floor(Math.random() * eleccionCantidad)
+  let compuEleccion = eleccion[numeroAzar]
+  let resultadoFinal = document.getElementById("resultadoFinal")
 
-if (usuarioEleccion == compuEleccion){
-    document.write("Empate")
-} else if (compuEleccion == "Piedra"){
-    if(usuarioEleccion == "Papel"){
-        document.write("Felicidades, ganaste!")
-    } else if(usuarioEleccion == "Tijera"){
-        alert("Mala suerte, perdiste. Querés volver a intentarlo?")
+  if (usuarioEleccion == compuEleccion) {
+    resultadoFinal.innerHTML = "Empate"
+  } else if (compuEleccion == "Piedra") {
+    if (usuarioEleccion == "Papel") {
+      resultadoFinal.innerHTML = "Felicidades, ganaste!"
+    } else if (usuarioEleccion == "Tijera") {
+      alert("Mala suerte, perdiste. Queres volver a intentar?")
+        
     }
 
-}else if(compuEleccion == "Papel"){
-        if(usuarioEleccion == "Tijera"){
-            document.write("Felicidades, ganaste!")
-        } else if(usuarioEleccion == "Piedra"){
-        alert("Mala suerte, perdiste. Querés volver a intentarlo?")
-        }
-    }else if(compuEleccion == "Tijera"){
-        if(usuarioEleccion == "Piedra"){
-            document.write("Felicidades, ganaste!")
-        } else if(usuarioEleccion == "Papel"){
-        alert("Mala suerte, perdiste. Querés volver a intentarlo?")
-        }
-    }
+  } else if (compuEleccion == "Papel") {
+    if (usuarioEleccion == "Tijera") {
+      resultadoFinal.innerHTML = "Felicidades, ganaste!"
+    } else if (usuarioEleccion == "Piedra") {
+      alert("Mala suerte, perdiste. Queres volver a intentar?")
+        
+      }
+    }else if (compuEleccion == "Tijera") {
+    if (usuarioEleccion == "Piedra") {
+      resultadoFinal.innerHTML = "Felicidades, ganaste!"
+    } else if (usuarioEleccion == "Papel") {
+      alert("Mala suerte, perdiste. Queres volver a intentar?")
 
+      }
+    }
+  }
